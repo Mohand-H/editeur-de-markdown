1705ac73f007a5a1ed771509ec2f8bd6e7c788ad
Mohand H <mohandhassam1@gmail.com>
2019-01-17 09:44:24 +0100

    utiliser et modifier le State

--- editeur-app/src/App.js
+++ editeur-app/src/App.js
@@ -3,14 +3,24 @@ import './App.css'
 import { sampleText } from './sampleText'
 
 class App extends Component {
+  state = {
+    text: sampleText
+  }
+
+  handleChange = event => {
+    const text = event.target.value
+    this.setState({ text })
+  }
+
   render() {
     return (
       <div className='container'>
         <div className='row'>
           <div className='col-sm-6'>
             <textarea
-              value={sampleText}
-              className='form-control' 
+              onChange={this.handleChange}
+              value={this.state.text}
+              className='form-control'
               rows='35' />
           </div>
           <div className='col-sm-6'>
