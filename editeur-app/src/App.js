c4f796a997a97b110cae0ab5292dd2f5222743b2
Mohand H <mohandhassam1@gmail.com>
2019-01-17 17:01:15 +0100

    editeur au top

--- editeur-app/src/App.js
+++ editeur-app/src/App.js
@@ -8,18 +8,34 @@ class App extends Component {
     text: sampleText
   }
 
+  componentDidMount() {
+    const text = localStorage.getItem('text')
+    if (text) {
+      this.setState({ text })
+    }else{
+      this.setState({ text: sampleText })
+    }
+ 
+
+  }
+
+  componentDidUpdate() {
+    const { text } = this.state
+    localStorage.setItem('text', text)
+  }
+
   handleChange = event => {
     const text = event.target.value
     this.setState({ text })
   }
 
-  renderText = text => { 
-   const __html =  marked(text, { sanitize: true})
-  
-   return {__html}
+  renderText = text => {
+    const __html = marked(text, { sanitize: true })
+
+    return { __html }
   }
 
-  
+
 
   render() {
     return (
@@ -33,7 +49,7 @@ class App extends Component {
               rows='35' />
           </div>
           <div className='col-sm-6'>
-            <div dangerouslySetInnerHTML={this.renderText(this.state.text) } /> 
+            <div dangerouslySetInnerHTML={this.renderText(this.state.text)} />
           </div>
         </div>
       </div>
